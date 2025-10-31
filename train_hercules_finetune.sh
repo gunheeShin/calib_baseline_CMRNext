0db0e5ffb24d66909c4f3755559ed71407aeb216
gml78905 <gml78905@kaist.ac.kr>
2025-10-31 14:24:06 +0900

    hercules

--- train_hercules_finetune.sh
+++ train_hercules_finetune.sh
@@ -1,5 +1,1 @@
-python3 train_calibration.py --savemodel /ws/data/iter1/ --data_folder_custom /ws/data --custom --max_r 20 --max_t 1.5 --weights /ws/data/KITTI/cmrnext_calibration_weights/cmrnext-calib-LEnc-iter1.tar --finetune --sensor_type radar --batch_size 4
-
-python3 train_calibration.py --savemodel /ws/data/iter1/ --data_folder_custom /ws/data --custom --max_r 1 --max_t 0.1 --weights /ws/data/KITTI/cmrnext_calibration_weights/cmrnext-calib-LEnc-iter1.tar --finetune --sensor_type radar --batch_size 4
-
-python3 train_calibration.py --savemodel /ws/data/iter1/ --data_folder_custom /ws/data --custom --max_r 0.2 --max_t 0.05 --weights /ws/data/KITTI/cmrnext_calibration_weights/cmrnext-calib-LEnc-iter1.tar --finetune --sensor_type radar --batch_size 4
+python3 train_calibration.py --savemodel /ws/checkpoint/hercules_checkpoints/ --data_folder_custom /ws/data/LG_Innotek/PublicDataset/hercules --custom --max_r 20 --max_t 1.5 --weights /ws/checkpoint/cmrnext-calib-LEnc-iter1.tar --finetune --sensor_type radar --epochs 50 --BASE_LEARNING_RATE 3e-5 --batch_size 4 --downsize
